7b729efb6a151857d2ba216571598a85bafaf2c0
kevintriplett <kt@kevintriplett.com>
2025-07-02 07:42:14 -0500

    update schema.sql

--- apps/backend/migrations/schema.sql
+++ apps/backend/migrations/schema.sql
@@ -2,12 +2,13 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 15.2 (Debian 15.2-1.pgdg110+1)
--- Dumped by pg_dump version 15.2 (Debian 15.2-1.pgdg110+1)
+-- Dumped from database version 17.4 (Postgres.app)
+-- Dumped by pg_dump version 17.5 (Homebrew)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', 'public', false);
@@ -56,12 +57,6 @@ CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;
 --
 
 COMMENT ON EXTENSION postgis IS 'PostGIS geometry, geography, and raster spatial types and functions';
-
-
---
--- Name: delete_user(integer); Type: PROCEDURE; Schema: public; Owner: -
---
-
 
 
 SET default_tablespace = '';
@@ -510,6 +505,7 @@ CREATE TABLE public.context_widgets (
     parent_id bigint,
     view character varying(255),
     icon character varying(255),
+    auto_added boolean DEFAULT false,
     view_group_id bigint,
     view_post_id bigint,
     custom_view_id bigint,
@@ -517,7 +513,6 @@ CREATE TABLE public.context_widgets (
     view_chat_id bigint,
     created_at timestamp with time zone,
     updated_at timestamp with time zone,
-    auto_added boolean DEFAULT false,
     CONSTRAINT single_view_reference CHECK (((((((
 CASE
     WHEN (view_group_id IS NOT NULL) THEN 1
@@ -1347,7 +1342,9 @@ CREATE TABLE public.groups (
     steward_descriptor_plural character varying(255) DEFAULT NULL::character varying,
     about_video_uri character varying(255),
     allow_in_public boolean DEFAULT false,
-    purpose text
+    purpose text,
+    welcome_page text,
+    website_url text
 );
 
 
@@ -1758,11 +1755,11 @@ CREATE TABLE public.moderation_actions (
     text text,
     reporter_id bigint NOT NULL,
     post_id bigint NOT NULL,
+    group_id bigint,
     status text,
     anonymous text,
     created_at timestamp with time zone,
-    updated_at timestamp with time zone,
-    group_id bigint
+    updated_at timestamp with time zone
 );
 
 
@@ -2039,8 +2036,7 @@ CREATE SEQUENCE public.org_seq
 
 CREATE TABLE public.platform_agreements (
     id integer NOT NULL,
-    text text,
-    type text
+    text text
 );
 
 
@@ -5472,14 +5468,6 @@ ALTER TABLE ONLY public.media
 
 ALTER TABLE ONLY public.groups_posts
     ADD CONSTRAINT fk_post_community_community_02 FOREIGN KEY (community_id) REFERENCES public.communities(id) DEFERRABLE INITIALLY DEFERRED;
-
-
---
--- Name: groups_posts fk_post_community_post_01; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.groups_posts
-    ADD CONSTRAINT fk_post_community_post_01 FOREIGN KEY (post_id) REFERENCES public.posts(id) DEFERRABLE INITIALLY DEFERRED;
 
 
 --
